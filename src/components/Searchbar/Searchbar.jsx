dce8017a5df7dd3bafe928b34b3b47795606b5a4
number-nine <sergii.pokhylko@gmail.com>
2023-05-28 18:40:14 +0300

    serachbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,66 +1,61 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import css from './Searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    query: '',
+const Searchbar = ({ isLoading, onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChangeQuery = e => {
+    const { value } = e.target;
+    setQuery(value);
   };
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.isQueryValid()) {
-      this.props.onSubmit(this.state.query.trim());
-      this.clearQuery();
+    if (isQueryValid()) {
+      onSubmit(query.trim());
+      clearQuery();
       return;
     }
     Notify.failure('Enter valid query');
   };
 
-  isQueryValid = () => {
-    return this.state.query.trim() !== '' ? true : false;
+  const isQueryValid = () => {
+    return query.trim() !== '' ? true : false;
   };
 
-  clearQuery = () => {
-    this.setState({ query: '' });
+  const clearQuery = () => {
+    setQuery('');
   };
 
-  render() {
-    const { isLoading } = this.props;
-      return(
-      <header className={css.Searchbar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button
-            type="submit"
-            className={css.SearchFormButton}
-            disabled={isLoading}
-          >
-            <span className={css.SearchFormButtonLabel}>Search</span>
-          </button>
+  return (
+    <header className={css.Searchbar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button
+          type="submit"
+          className={css.SearchFormButton}
+          disabled={isLoading}
+        >
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
 
-          <input
-            className={css.SearchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="query"
-            value={this.state.query}
-            onChange={this.handleChange}
-            disabled={isLoading}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="query"
+          value={query}
+          onChange={handleChangeQuery}
+          disabled={isLoading}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
