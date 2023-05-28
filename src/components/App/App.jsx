c71ee0ec0564c91a35842b6693fc58c598f86643
number-nine <sergii.pokhylko@gmail.com>
2023-05-28 19:26:41 +0300

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,46 +1,72 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Searchbar from 'components/Searchbar';
 import ImageGallery from 'components/ImageGallery';
 import css from './App.module.css';
 
-class App extends Component {
-  state = {
-    query: '',
-    isLoading: false,
-  };
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  handleQuery = query => {
-    this.setState({
-      query,
-    });
-    this.setLoadingStatus();
-  };
+   const handleQuery = query => {
+      setQuery(query);
+      setIsLoading(true);
+    };
 
-  setLoadingStatus = () => {
-    this.setState({
-      isLoading: true,
-    });
-  };
-
-  unsetLoadingStatus = () => {
-    this.setState({ isLoading: false });
-  };
-
-  render() {
-    const { query, isLoading } = this.state;
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.handleQuery} isLoading={isLoading} />
-        <ImageGallery
-          query={query}
-          isLoading={isLoading}
-          onLoadingStart={this.setLoadingStatus}
-          onLoadingComplete={this.unsetLoadingStatus}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleQuery} isLoading={isLoading} />
+      <ImageGallery
+        query={query}
+        isLoading={isLoading}
+        onLoadingStart={() => {
+          setIsLoading(true);
+        }}
+        onLoadingComplete={() => {
+          setIsLoading(false);
+        }}
+      />
+    </div>
+  );
 }
+
+// class App extends Component {
+//   state = {
+//     query: '',
+//     isLoading: false,
+//   };
+
+//   handleQuery = query => {
+//     this.setState({
+//       query,
+//     });
+//     this.setLoadingStatus();
+//   };
+
+//   setLoadingStatus = () => {
+//     this.setState({
+//       isLoading: true,
+//     });
+//   };
+
+//   unsetLoadingStatus = () => {
+//     this.setState({ isLoading: false });
+//   };
+
+//   render() {
+//     const { query, isLoading } = this.state;
+//     return (
+//       <div className={css.App}>
+//         <Searchbar onSubmit={this.handleQuery} isLoading={isLoading} />
+//         <ImageGallery
+//           query={query}
+//           isLoading={isLoading}
+//           onLoadingStart={this.setLoadingStatus}
+//           onLoadingComplete={this.unsetLoadingStatus}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
