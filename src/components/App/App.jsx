36307466329053c3b816de6f03498242b7c0b622
number-nine <sergii.pokhylko@gmail.com>
2023-05-29 00:21:58 +0300

    start imageGallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -30,43 +30,4 @@ const App = () => {
   );
 }
 
-// class App extends Component {
-//   state = {
-//     query: '',
-//     isLoading: false,
-//   };
-
-//   handleQuery = query => {
-//     this.setState({
-//       query,
-//     });
-//     this.setLoadingStatus();
-//   };
-
-//   setLoadingStatus = () => {
-//     this.setState({
-//       isLoading: true,
-//     });
-//   };
-
-//   unsetLoadingStatus = () => {
-//     this.setState({ isLoading: false });
-//   };
-
-//   render() {
-//     const { query, isLoading } = this.state;
-//     return (
-//       <div className={css.App}>
-//         <Searchbar onSubmit={this.handleQuery} isLoading={isLoading} />
-//         <ImageGallery
-//           query={query}
-//           isLoading={isLoading}
-//           onLoadingStart={this.setLoadingStatus}
-//           onLoadingComplete={this.unsetLoadingStatus}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import getPictures from 'controllers/api-controller';
@@ -7,94 +7,173 @@ import Button from 'components/Button';
 import Loader from 'components/Loader';
 import css from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  state = {
-    currentData: null,
-    page: 1,
-    total: 0,
-    perPage: 12,
-    canLoadMore: false,
-  };
+const perPage = 12;
 
-  async componentDidUpdate(_, {page:prevPage, currentData:prevData}) {
-    const { query } = this.props;
-    const { page, perPage, currentData, total } = this.state;
+const ImageGallery = ({
+  isLoading,
+  query,
+  onLoadingStart,
+  onLoadingComplete,
+}) => {
+  const [currentData, setCurrentData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [canLoadMore, setCanLoadMore] = useState(false);
 
-    if (prevPage === page && prevData === currentData) {
-      const { hits, total } = await getPictures({
-        page: 1,
-        query,
-        perPage,
-      });
+  // useEffect(() => {
 
-      this.setState({
-        currentData: hits,
-        page: 1,
-        total,
-        canLoadMore: total > perPage,
-      });
+  // },[query])
 
-      this.props.onLoadingComplete();
-
+  useEffect(() => {
+    if (!query) {
       return;
     }
-
-    if (page > prevPage) {
-      const { hits } = await getPictures({
+    const fetchData = async (query, page, perPage) => {
+      const { hits, total } = await getPictures({
         page,
         query,
         perPage,
       });
-      this.setState({
-        currentData: [...currentData, ...hits],
-        canLoadMore:
-          total - perPage * page > 0,
-      });
+      return [hits, total];
+    };
 
-      this.props.onLoadingComplete();
+    fetchData(query, page, perPage)
+      .then(([data, count]) => {
+        setCurrentData(state => [...state, ...data]);
+        setTotal(count);
+        setCanLoadMore(total > page * perPage);
+      })
+      .catch(() => {
+        console.log('Something went wrong');
+      })
+      .finally(onLoadingComplete);
+  }, [page, total, onLoadingComplete, query]);
 
-      return;
-    }
-  }
+  useEffect(() => {
+    setCurrentData([]);
+  }, [query]);
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-    this.props.onLoadingStart();
+  const handleLoadMore = () => {
+    setPage(state => state + 1);
+    onLoadingStart();
   };
 
-  render() {
-    const { isLoading, query } = this.props;
-    const { currentData, canLoadMore } = this.state;
-    return (
-      <>
-        {!currentData?.length && (
-          <p className={css.ImageGalleryMessage}>Nothing to show</p>
-        )}
-        <ul className={css.ImageGallery}>
-          {currentData &&
-            currentData.map(picture => (
-              <ImageGalleryItem
-                key={picture.id}
-                src={picture.webformatURL}
-                alt={picture.tags}
-                image={picture.largeImageURL}
-              />
-            ))}
-        </ul>
-        {canLoadMore && (
-          <Button
-            onLoadMore={this.handleLoadMore}
-            isLoading={isLoading}
-            query={query}
-          />
-        )}
-        {isLoading && <Loader />}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {!currentData.length && (
+        <p className={css.ImageGalleryMessage}>Nothing to show</p>
+      )}
+      <ul className={css.ImageGallery}>
+        {Boolean(currentData.length) &&
+          currentData.map(picture => (
+            <ImageGalleryItem
+              key={picture.id}
+              src={picture.webformatURL}
+              alt={picture.tags}
+              image={picture.largeImageURL}
+            />
+          ))}
+      </ul>
+      {canLoadMore && (
+        <Button
+          onLoadMore={handleLoadMore}
+          isLoading={isLoading}
+          query={query}
+        />
+      )}
+      {isLoading && <Loader />}
+    </>
+  );
+};
+
+// class ImageGallery extends Component {
+//   state = {
+//     currentData: null,
+//     page: 1,
+//     total: 0,
+//     perPage: 12,
+//     canLoadMore: false,
+//   };
+
+//   async componentDidUpdate(_, {page:prevPage, currentData:prevData}) {
+//     const { query } = this.props;
+//     const { page, perPage, currentData, total } = this.state;
+
+//     if (prevPage === page && prevData === currentData) {
+//       const { hits, total } = await getPictures({
+//         page: 1,
+//         query,
+//         perPage,
+//       });
+
+//       this.setState({
+//         currentData: hits,
+//         page: 1,
+//         total,
+//         canLoadMore: total > perPage,
+//       });
+
+//       this.props.onLoadingComplete();
+
+//       return;
+//     }
+
+//     if (page > prevPage) {
+//       const { hits } = await getPictures({
+//         page,
+//         query,
+//         perPage,
+//       });
+//       this.setState({
+//         currentData: [...currentData, ...hits],
+//         canLoadMore:
+//           total - perPage * page > 0,
+//       });
+
+//       this.props.onLoadingComplete();
+
+//       return;
+//     }
+//   }
+
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//     this.props.onLoadingStart();
+//   };
+
+//   render() {
+//     const { isLoading, query } = this.props;
+//     const { currentData, canLoadMore } = this.state;
+//     return (
+//       <>
+//         {!currentData?.length && (
+//           <p className={css.ImageGalleryMessage}>Nothing to show</p>
+//         )}
+//         <ul className={css.ImageGallery}>
+//           {currentData &&
+//             currentData.map(picture => (
+//               <ImageGalleryItem
+//                 key={picture.id}
+//                 src={picture.webformatURL}
+//                 alt={picture.tags}
+//                 image={picture.largeImageURL}
+//               />
+//             ))}
+//         </ul>
+//         {canLoadMore && (
+//           <Button
+//             onLoadMore={this.handleLoadMore}
+//             isLoading={isLoading}
+//             query={query}
+//           />
+//         )}
+//         {isLoading && <Loader />}
+//       </>
+//     );
+//   }
+// }
 
 export default ImageGallery;
 
